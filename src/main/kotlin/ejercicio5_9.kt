5b598648c33fd7eb09f5a2411738b302dfe82094
3kxhoDark <jmcumbrera@iesrafaelalberti.es>
2021-10-25 21:33:15 +0200

    Commit

--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -6,10 +6,19 @@ open class Cuenta (open val numCuenta: String, open var saldo: Double) {
     open fun realizarPago(pago: Double): Double {}
 }
 
-class Persona(val DNI: String, var accounts: Array<Cuenta>) {
+class Persona(val DNI: String) {
+    private var accounts: Array<Cuenta?> = arrayOfNulls<Cuenta>(3)
+
     private var numCuentas = 0
     fun addCuenta(account: Cuenta): Boolean {
-        accounts.add(account)
+        var i: Int = 0
+        return if (i < 4) {
+            accounts[i] = account
+            i++
+            true
+        } else {
+            false
+        }
     }
     fun esMorosa(): Boolean {}
 }
@@ -18,7 +27,7 @@ fun main() {
     /**
      * Ejercicio 5.9
      */
-    //val p = Persona("49078667W")
-    //p.addCuenta("45931857AC",0.0)
+    val p = Persona("49078667W")
+    p.addCuenta("45931857AC")
     //p.addCuenta("53759257RV",700.0)
 }
